a6261f0e1090d417f94cde29a813517dbad8b5fd
Foxtrot <foxtrotnull@gmail.com>
2016-07-11 22:39:11 +0100

    :tada: Initial Commit

---
+++ api/module.php
@@ -0,0 +1,142 @@
+<?php namespace pineapple;
+
+/***
+Modem Manager <api/module.php>
+Written by Foxtrot <foxtrotnull@gmail.com>
+Distributed under the MIT Licence <https://opensource.org/licenses/MIT>
+
+This PHP file handles the calls to the system recieved from the AngularJS (js/module.js)
+and will send data to the JS.
+***/
+
+class ModemManager extends Module
+{
+    public function route()
+    {
+        switch ($this->request->action) {
+            /* If the requested action from the JS is 'getUSB', run the getUSB() function
+               Repeat for each case... */
+            case 'getUSB':
+                $this->getUSB();
+                break;
+
+            case 'getTTYs':
+                $this->getTTYs();
+                break;
+
+            case 'loadConfiguration':
+                $this->loadConfiguration();
+                break;
+
+            case 'saveConfiguration':
+                $this->saveConfiguration();
+                break;
+
+            case 'resetConfiguration':
+                $this->resetConfiguration();
+                break;
+        }
+    }
+
+    private function getUSB()
+    {
+        /* Execute 'lsusb' and capture its output in the $lsusb variable.
+           Then split the output by its newlines. */
+        exec('lsusb', $lsusb);
+        $lsusb = implode("\n", $lsusb);
+
+        /* Send the response 'lsusb' back to JS with the variable $lsusb as content. */
+        $this->response = array('lsusb' => $lsusb);
+    }
+
+    private function getTTYs()
+    {
+        /* Get the listing of /dev/ttyUSB* and store it as %TTYs. */
+        exec('ls /dev/ttyUSB*', $TTYs);
+
+        /* If the variable $TTYs is empty, send back a response:
+           'success' as false, and 'availableTTYs' as false.
+
+           Else send 'success' as true, and 'availableTTYs' as the content of $TTYs */
+        if (empty($TTYs)) {
+            $this->response = array('success' => false,
+                                    'availableTTYs' => false);
+        } else {
+            $TTYs = implode("\n", $TTYs);
+            $this->response = array('success' => true,
+                                    'availableTTYs' => $TTYs);
+        }
+    }
+
+    private function loadConfiguration()
+    {
+        /* For easier code reading, assign a variable for each bit of information we require from the system.
+           Read more about UCI at https://wiki.openwrt.org/doc/uci */
+        $interface     = $this->uciGet('network.wan2.ifname');
+        $protocol      = $this->uciGet('network.wan2.proto');
+        $service       = $this->uciGet('network.wan2.service');
+        $device        = $this->uciGet('network.wan2.device');
+        $apn           = $this->uciGet('network.wan2.apn');
+        $username      = $this->uciGet('network.wan2.username');
+        $password      = $this->uciGet('network.wan2.password');
+        $dns           = $this->uciGet('network.wan2.dns');
+        $peerdns       = $this->uciGet('network.wan2.peerdns');
+        $pppredial     = $this->uciGet('network.wan2.ppp_redial');
+        $defaultroute  = $this->uciGet('network.wan2.defaultroute');
+        $keepalive     = $this->uciGet('network.wan2.keepalive');
+        $pppdoptions   = $this->uciGet('network.wan2.pppd_options');
+
+        $this->response = array('success'      => true,
+                                'interface'    => $interface,
+                                'protocol'     => $protocol,
+                                'service'      => $service,
+                                'device'       => $device,
+                                'apn'          => $apn,
+                                'username'     => $username,
+                                'password'     => $password,
+                                'dns'          => $dns,
+                                'peerdns'      => $peerdns,
+                                'pppredial'    => $pppredial,
+                                'defaultroute' => $defaultroute,
+                                'keepalive'    => $keepalive,
+                                'pppdoptions'  => $pppdoptions);
+    }
+
+    private function saveConfiguration()
+    {
+        $interface     = $this->request->interface;
+        $protocol      = $this->request->protocol;
+        $service       = $this->request->service;
+        $device        = $this->request->device;
+        $apn           = $this->request->apn;
+        $username      = $this->request->username;
+        $password      = $this->request->password;
+        $dns           = $this->request->dns;
+        $peerdns       = $this->request->peerdns;
+        $pppredial     = $this->request->pppredial;
+        $defaultroute  = $this->request->defaultroute;
+        $keepalive     = $this->request->keepalive;
+        $pppdoptions   = $this->request->pppdoptions;
+
+        $this->response = array('success' => true);
+    }
+
+    private function resetConfiguration()
+    {
+        $interface     = $this->request->interface;
+        $protocol      = $this->request->protocol;
+        $service       = $this->request->service;
+        $device        = $this->request->device;
+        $apn           = $this->request->apn;
+        $username      = $this->request->username;
+        $password      = $this->request->password;
+        $dns           = $this->request->dns;
+        $peerdns       = $this->request->peerdns;
+        $pppredial     = $this->request->pppredial;
+        $defaultroute  = $this->request->defaultroute;
+        $keepalive     = $this->request->keepalive;
+        $pppdoptions   = $this->request->pppdoptions;
+
+        $this->response = array('success' => true);
+    }
+}
---
+++ js/module.js
@@ -0,0 +1,104 @@
+registerController('USBController', ['$api', '$scope', function($api, $scope) {
+    $scope.lsusb = "";
+    $scope.availableTTYs = "";
+
+    $api.request({
+        module: 'ModemManager',
+        action: 'getUSB'
+    }, function(response) {
+        if (response.lsusb) {
+            $scope.lsusb = response.lsusb;
+        }
+    });
+
+    $api.request({
+        module: 'ModemManager',
+        action: 'getTTYs'
+    }, function(response) {
+        if (response.success) {
+            $scope.availableTTYs = response.availableTTYs;
+        } else {
+            $scope.availableTTYs = "No TTYs Found...";
+        }
+    });
+}]);
+
+registerController('ModemController', ['$api', '$scope', '$timeout', function($api, $scope, $timeout) {
+    $scope.savedConfiguration = false;
+    $scope.resetConfigurationSuccess = false;
+    $scope.saveConfigurationError = "";
+    $scope.loadConfigurationError = "";
+    $scope.resetConfigurationError = "";
+
+    $scope.loadConfiguration = (function() {
+        $api.request({
+            module: 'ModemManager',
+            action: 'loadConfiguration'
+        }, function(response) {
+            if (response.success === true) {
+                $scope.interface = response.interface;
+                $scope.protocol = response.protocol;
+                $scope.service = response.service;
+                $scope.device = response.device;
+                $scope.apn = response.apn;
+                $scope.username = response.username;
+                $scope.password = response.password;
+                $scope.dns = response.dns;
+                $scope.peerdns = response.peerdns;
+                $scope.pppredial = response.pppredial;
+                $scope.defaultroute = response.defaultroute;
+                $scope.keepalive = response.keepalive;
+                $scope.pppdoptions = response.pppdoptions;
+            } else {
+                $scope.loadConfigurationError = "Failed to load configuration.";
+            }
+        });
+    });
+
+    $scope.saveConfiguration = (function() {
+        $api.request({
+            module: 'ModemManager',
+            action: 'saveConfiguration',
+            interface: $scope.interfaceName,
+            protocol: $scope.protocol,
+            service: $scope.service,
+            device: $scope.device,
+            apn: $scope.apn,
+            username: $scope.username,
+            password: $scope.password,
+            dns: $scope.dns,
+            peerdns: $scope.peerdns,
+            pppredial: $scope.pppredial,
+            defaultroute: $scope.defaultroute,
+            keepalive: $scope.keepalive,
+            pppdoptions: $scope.pppdoptions
+        }, function(response) {
+            if (response.success === true) {
+                $scope.savedConfiguration = true;
+                $timeout(function(){
+                    $scope.savedConfiguration = false;
+                }, 2000);
+            } else {
+                $scope.saveConfigurationError = "Failed to save configuration.";
+            }
+        });
+    });
+
+    $scope.resetConfiguration = (function() {
+        $api.request({
+            module: 'ModemManager',
+            action: 'resetConfiguration'
+        }, function(response) {
+            if (response.success === true) {
+                $scope.resetConfigurationSuccess = true;
+                $timeout(function(){
+                    $scope.resetConfigurationSuccess = false;
+                }, 2000);
+            } else {
+                $scope.resetConfigurationError = "Failed to open configuration.";
+            }
+        });
+    });
+
+    $scope.loadConfiguration();
+}]);
